c1c8ab35d9c876e2a3ce419f0ec9f53c468a0a47
R3IS <101276269+xreiz0r@users.noreply.github.com>
2022-09-26 12:51:20 +0100

    Add files via upload

---
+++ C Primer (5ᵗʰ) 2012/ex1-12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i = 0;
+
+for(int j = 1;j <=100;j++){
+ i += j;
+ //} single answer
+ 
+ cout << " the Sum of 1 to 100 inclusive is : " << i << endl;
+} //process answer
+
+
+return 0;
+}
+// Final answer is 0 from single answer.
